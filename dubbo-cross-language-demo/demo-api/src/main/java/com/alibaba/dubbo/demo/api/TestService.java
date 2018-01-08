15fe0bf6dc7266ec20ffb071d291af6db45d6f85
孔令圳 <konglingzhen@douyu.tv>
2018-01-08 17:56:47 +0800

    restful api demo

--- dubbo-cross-language-demo/demo-api/src/main/java/com/alibaba/dubbo/demo/api/TestService.java
+++ dubbo-cross-language-demo/demo-api/src/main/java/com/alibaba/dubbo/demo/api/TestService.java
@@ -4,36 +4,36 @@ import java.util.Map;
 
 public interface TestService {
 
-TestRes invokeVoid();
+    TestRes invokeVoid();
 
-TestRes invokeSingleString(String single);
+    TestRes invokeSingleString(String single);
 
-TestRes invokeSingleInteger(Integer num);
+    TestRes invokeSingleInteger(Integer num);
 
-TestRes invokeSingleInt(int num);
+    TestRes invokeSingleInt(int num);
 
-TestRes invokeSingleFloat(Float num);
+    TestRes invokeSingleFloat(Float num);
 
-TestRes invokeSingleFlo(float num);
+    TestRes invokeSingleFlo(float num);
 
-TestRes invokeSingleBoolean(Boolean bool);
+    TestRes invokeSingleBoolean(Boolean bool);
 
-TestRes invokeSingleBool(boolean bool);
+    TestRes invokeSingleBool(boolean bool);
 
-TestRes invokeArray(String[] values);
+    TestRes invokeArray(String[] values);
 
-TestRes invokeObject(TestReq req);
+    TestRes invokeObject(TestReq req);
 
-TestRes invokeObjects(TestReq req, NameReq nameReq, DepartmentReq departmentReq);
+    TestRes invokeObjects(TestReq req, NameReq nameReq, DepartmentReq departmentReq);
 
-TestRes invokeMap(Map<String, String> map);
+    TestRes invokeMap(Map<String, String> map);
 
-TestRes invokeStringAndArray(String single, String[] values);
+    TestRes invokeStringAndArray(String single, String[] values);
 
-TestRes invokeObjectAndString(TestReq req, String single);
+    TestRes invokeObjectAndString(TestReq req, String single);
 
-TestRes invokeArrayAndObjectAndString(String[] values, TestReq req, String single);
+    TestRes invokeArrayAndObjectAndString(String[] values, TestReq req, String single);
 
-TestRes invokeMapAndObjectAndArrayAndString(Map<String, String> map, TestReq req, String[] values, String single);
+    TestRes invokeMapAndObjectAndArrayAndString(Map<String, String> map, TestReq req, String[] values, String single);
 
 }
